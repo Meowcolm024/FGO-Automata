4fa93fa83f1eb765d57e668ef068dd43f0847c1d
Meowcolm024 <luoyicong1026@live.com>
2019-12-28 08:23:41 +0800

    added dmg calc

--- core/Card.py
+++ core/Card.py
@@ -1,25 +1,53 @@
 class Card():
-    def __init__(self, mrk: float, clr: int, prt: int, eb: bool):
+    def __init__(self, idt: int, mrk: float, color: int):
         """
         Parameters
         ----------
             mrk: float
         Resist: 0.5, Normal: 1, Weak: 2
 
-            clr: int
-        Quick: 1, Arts:2, Buster: 3
+            color: int
+        Quick: 0.8, Arts:1, Buster: 2
+        """
+        self.identity = idt
+        self.mark = mrk
+        self.atk = color
 
+    def get_atk(self, priority: int, exbuster: bool):
+        """
+        Parameters
+        ----------
             prt: int
-        Card priority 1 -> 1, 2 -> 1.2, 3 -> 1.4
+        Card priority 0 -> 1, 1 -> 1.2, 2 -> 1.4
 
             eb: bool
         Buster first -> atk+0.5
         """
-        self.mark = mrk
-        self.color = clr
-        self.priority = prt
-        self.exbuster = eb
-        self.atk = 1
+        dmg = self.atk
 
-    def get_atk(self):
-        pass
+        if priority == 1:
+            dmg *= 1.2
+        elif priority == 2:
+            dmg *= 1.4
+
+        if exbuster:
+            dmg += 0.5
+
+        dmg *= self.mark
+
+        return dmg
+
+    def __eq__(self, value):
+        return True if self.identity == value.identity else False
+
+    def __str__(self):
+        return f"ID: {self.identity}, Mark: {self.mark}, Color: {self.atk}"
+
+
+if __name__ == "__main__":
+    t = Card(1, 2, 0.8)
+    q = Card(1, 0.5, 0.8)
+    s = Card(2, 1, 1)
+    print(t.get_atk(2, True))
+    print(t == s)
+    print(t)
--- core/Dynamica.py
+++ core/Dynamica.py
@@ -1,3 +1,25 @@
+from core.Card import Card
+
 class Dynamica():
-    def __init__(self):
-        super().__init__()
+    def __init__(self, crd: [Card]):
+        self.cards = crd
+
+    def read_cards(self):
+        pass
+
+    def arrange_cards(self) -> [Card]:
+        max_atk = 0
+        max_comb = []
+        for fst in self.cards:
+            for sec in self.cards:
+                if fst == sec: continue
+                for trd in self.cards:
+                    if sec == trd or fst == trd: continue
+                    ex = False
+                    if fst.atk == 2: ex = True
+                    cur_atk = sum([fst.get_atk(1, ex), sec.get_atk(1.2, ex), trd.get_atk(1.4, ex)])
+                    if cur_atk > max_atk:
+                        max_atk = cur_atk
+                        max_comb = [fst, sec, trd]
+        print(max_atk)
+        return max_comb

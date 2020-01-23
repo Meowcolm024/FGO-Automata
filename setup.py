e69da2f73d01c6826c2dd687d1f263a97b29ef10
Meowcolm024 <luoyicong1026@live.com>
2020-01-24 07:07:15 +0800

    parpare for servant reco

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FGO-Automata',
+    version='0.5.0',
+    packages=['core'],
+    url='',
+    license='MIT',
+    author='Meowcolm024',
+    author_email='',
+    description=''
+)
